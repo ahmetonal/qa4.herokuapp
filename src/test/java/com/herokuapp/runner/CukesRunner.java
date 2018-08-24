65e572cc1ceb3daf626174497db151e175624388
aslisahin <a_turkal@yahoo.com>
2018-08-24 09:54:06 -0500

    commit with DB Utility

--- src/test/java/com/herokuapp/runner/CukesRunner.java
+++ src/test/java/com/herokuapp/runner/CukesRunner.java
@@ -13,19 +13,12 @@ import cucumber.api.junit.Cucumber;
 				"json:target/cucumber.json"
 		
 		},
-<<<<<<< HEAD
 		
 		tags="@asli",
 		features="src/test/resources/com/qa4.herokuapp/features", 
 		glue="com/herokuapp/step_definitions"
 //		,dryRun =true
-=======
-		 
-		tags="@talha",
-		features="src/test/resources/com/qa4.herokuapp/features", 
-		glue="com/herokuapp/step_definitions"
-		,dryRun = false
-		,monochrome = true
->>>>>>> 2f60bd095e71eb1378350fe78f66b55a3bda67f6
+	//	,monochrome = true
+
 		)
 public class CukesRunner {}
